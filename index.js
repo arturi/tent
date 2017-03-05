8f56f67c45cc1e236ebede67555eda71448ea2dc
Artur Paikin <artur@arturpaikin.com>
2017-03-05 12:35:23 -0500

    mount editor element `oncreate`, no two-way binding, log function

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-// const fieldEditor = require('./field-editor')
 const simpleEditor = require('./simple-editor')
 const markdownPreview = require('./markdown-preview')
 const api = require('./client-api.js')
 const debounce = require('lodash.debounce')
-// const throttle = require('lodash/throttle')
 const csjs = require('csjs')
-// const html = require('choo/html')
-// const choo = require('choo')
 
 const { h, app } = require('hyperapp')
 const hyperx = require('hyperx')
 const html = hyperx(h)
+
+// var dragDrop = require('drag-and-drop-files')
+// var fileReader = require('filereader-stream')
+// var concat = require('concat-stream')
+// var path = require('path')
+//
+// dragDrop(window, function (files) {
+//   files.forEach(function (file) {
+//     fileReader(file).pipe(concat({ encoding: 'buffer' }, function (buf) {
+//       var ext = path.extname(file.name).slice(1)
+//       if (ext === 'svg') ext = 'svg+xml'
+//       var img = document.createElement('img')
+//       var src = 'data:image/' + ext + ';base64,' + buf.toString('base64')
+//       img.setAttribute('src', src)
+//       document.body.appendChild(img)
+//     }))
+//   })
+// })
+
+function log (msg) {
+  if (typeof msg === 'object') {
+    try {
+      msg = JSON.stringify(msg)
+    } catch (e) {
+      msg = msg
+    }
+  }
+
+  var date = new Date()
+  var hours = date.getHours().toString()
+  var minutes = date.getMinutes().toString()
+  var seconds = date.getSeconds().toString()
+  var time = hours + ':' + minutes + ':' + seconds
+
+  // const resultingMessage = `%c [${time}] ✨ ${msg}`
+  // console.log(resultingMessage, 'color: #5e2ca5;')
+  const resultingMessage = `[${time}] 🚦 ${msg}`
+  console.log(resultingMessage)
+}
 
 const styles = csjs`
   body, html {
@@ -74,7 +109,9 @@ const styles = csjs`
     overflow-y: auto;
   }
 `
-// const app = choo()
+
+let editor
+let editorEl
 
 const model = {
   doc: '',
@@ -85,7 +122,8 @@ const model = {
 
 const subscriptions = {
   initialize: (model, actions, error) => {
-    console.log('start like an animal')
+    // console.log('Start like an animal!')
+    log('Start like an animal!')
 
     window.onbeforeunload = (ev) => {
       actions.saveState()
@@ -113,7 +151,8 @@ const effects = {
     return new Promise(function (resolve, reject) {
       api.getList((err, res) => {
         if (err) reject(err)
-        console.log('loaded doc list')
+        // console.log('Loaded doc list')
+        log('Loaded doc list')
         actions.updateDocList(res.data)
         return resolve(res.data)
       })
@@ -124,75 +163,82 @@ const effects = {
     return new Promise(function (resolve, reject) {
       api.getDoc(docId, (err, res) => {
         if (err) reject(err)
-        console.log('loaded doc')
+        // console.log('Loaded doc:', docId)
+        log('Loaded doc: ' + docId)
         actions.updateDoc({ doc: res.data, docId: docId })
+        actions.updateEditor(model, actions, data, error)
         resolve(res)
       })
     })
   },
+  updateEditor: (model, actions, data, error) => {
+    log('Updating editor')
+    // console.log('Updating editor')
+    editor.update(model.doc, editorEl)
+  },
   saveDoc: (model, actions, data, error) => {
-    console.log('saving...')
-    console.log(data.doc)
+    // console.log('Saving:', data.docId)
+    log('Saving: ' + data.docId)
     return new Promise(function (resolve, reject) {
       api.saveDoc(data.docId, data.doc, (err, res) => {
         if (err) reject(err)
         console.log(res)
+        log(res)
         resolve(res)
       })
     })
   },
   newDoc: (model, actions, data, error) => {
     const docId = data
-    console.log('new doc', docId)
+    // console.log('Creating new doc:', docId)
+    log('Creating new doc: ' + docId)
 
     return actions.saveDoc({ docId: docId, doc: '' })
       .then(actions.loadDocList)
       .then(() => actions.loadDoc(docId))
       .then(actions.showNewDoc)
-
-    // actions.saveDoc({ docId: docId, doc: '' }), () => {
-    //   actions.loadDocList', () => {
-    //     actions.loadDoc', docId, () => {
-    //       send('showNewDoc', done)
-    //     })
-    //   })
-    // }
   },
   saveState: (model, actions, data, error) => {
     localStorage.setItem('tentState', JSON.stringify(model))
   },
   loadLastOpenDoc: (model, actions, data, error) => {
-    const savedState = JSON.parse(localStorage.getItem('tentState'))
-    console.log('yes, some saved state: ', savedState)
+    const savedStateString = localStorage.getItem('tentState')
+    const savedState = JSON.parse(savedStateString)
+    let docId
     if (savedState && savedState.docId) {
-      return actions.loadDoc(savedState.docId)
+      // console.log('Found some saved state:', savedState)
+      log('Found some saved state: ' + savedStateString)
+      docId = savedState.docId
     }
-    return actions.loadDoc()
+    return actions.loadDoc(docId)
+      .then(actions.updateEditor)
   }
 }
 
-function saveDoc (actions, data) {
-  // console.log(data)
-  actions.saveDoc(data)
-  // actions.updateDoc(data)
+const _saveDoc = debounce((data, actions) => actions.saveDoc(data), 1000)
+
+function Editor (model, actions) {
+  editor = simpleEditor(model.doc, (updatedDoc) => {
+    const data = {docId: model.docId, doc: updatedDoc}
+    actions.updateDoc(data)
+    _saveDoc(data, actions)
+  })
+
+  return html`<div oncreate=${(el) => {
+      el.appendChild(editor.el)
+      editorEl = el
+    }}>
+  </div>`
 }
-const debouncedSaveDoc = debounce(saveDoc, 1000)
 
 function view (model, actions) {
   let newDocName = ''
-
-  const editor = simpleEditor(model.doc, (updatedDoc) => {
-    const data = {docId: model.docId, doc: updatedDoc}
-    actions.updateDoc(data)
-    // actions.saveDoc(data)
-    // saveDoc(actions, data)
-  })
 
   return html`
     <main class=${styles.main}>
       ${model.showNewDoc
         ? html`<div>
-          <h4>create new document</h4>
+          <h4>Create new document</h4>
           <input type="text" placeholder="path/name" onchange=${(ev) => newDocName = ev.target.value}/>
           <button onclick=${() => actions.newDoc(newDocName)}>create</button>
         </div>`
@@ -207,9 +253,7 @@ function view (model, actions) {
         })}
       </ul>
       <div class="${styles.editor}">
-        <div oncreate=${(el) => {
-          el.appendChild(editor.el)
-        }} onupdate=${() => editor.update(model.doc)}></div>
+        ${Editor(model, actions)}
       </div>
       <div class="${styles.preview}">
         ${markdownPreview(model.doc, { parseFrontmatter: true } )}
@@ -218,11 +262,10 @@ function view (model, actions) {
   `
 }
 
-const myApp = app({ model: model, view: view, reducers: reducers, effects: effects, subscriptions: subscriptions})
-
-// function editMe (content) {
-//   const editorEl = simpleEditor(content, (updatedDoc) => {
-//     debouncedSaveAndUpdateDoc(actions, {docId: model.docId, updatedDoc: updatedDoc})
-//   })
-//   document.body.appendChild(editorEl)
-// }
+const myApp = app({
+  model: model,
+  view: view,
+  reducers: reducers,
+  effects: effects,
+  subscriptions: subscriptions
+})
